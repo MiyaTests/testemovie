d77c0c85a2b10cfeda4f90e8d877904fe6176829
lucaskmiyazaki <lucaskmiyazaki@hotmail.com>
2018-12-04 16:21:25 -0200

    versao2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+movies = [{title: "Alladin", release_date: "21-03-2000"}, {title: "Amelie Poulain", release_date: "25-05-2000"}]
+movies.each do |movie|
+  Movie.create(movie)
+end
+
+actors = [{name: "Angelina Jolie", gender: "female"}, {name: "Will Smith", gender: "male"}]
+actors.each do |actor|
+  Actor.create(actor)
+end  
